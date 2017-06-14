4212b0e9e9bce488d63f6abea5a817ce93a4ff34
PeterTsepalov <tsepalovp@gmail.com>
2017-06-14 12:04:05 -0700

    Initial check in

---
+++ ArrayPractice/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[2, 6]
+            {
+                { 10, 11, 12, 13, 14, 15 },
+                { 16, 17, 18, 19, 20, 21 }
+            };
+            Console.Write(array);
+        }
+    }
+}
