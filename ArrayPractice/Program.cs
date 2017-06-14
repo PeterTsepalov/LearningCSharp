e9e14eb13f0af151683413b59092dfe65e5476bf
Nikolai Tsepalov <tsepalov@yahoo.com>
2017-06-14 15:08:42 -0700

    Added Iteration template

--- ArrayPractice/Program.cs
+++ ArrayPractice/Program.cs
@@ -22,7 +22,16 @@ namespace ArrayPractice
         }
         static void Display(int[,] array)
         {
-            
+            //Iteration template
+            for (int i = 0; i < array.Rank; i++)
+            {
+                for (int j = 0; j < array.GetLength(i); j++)
+                {                    
+                    Console.WriteLine("{0} {1}", i,j);
+                }
+            }
+            int p = 711;
+            string padded = p.ToString().PadLeft(6); // Padding example: padded = "  711"; 
         }
 
         static void Main(string[] args)
@@ -30,10 +39,10 @@ namespace ArrayPractice
             int[] test = {2, 668888886, 8};
             Display(test);
             Console.WriteLine();
-            int[,] array = new int[2, 6]
+            int[,] array = new int[2, 3]
             {
-                { 10, 11, 12, 13, 14, 15 },
-                { 16, 17, 18, 19, 20, 21 }
+                { 10, 11, 12/*, 13, 14, 15*/ },
+                { 16, 17, 18 /*, 19, 20, 21*/ }
             };
             Display(array);
         }
