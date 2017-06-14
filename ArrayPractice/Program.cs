1e367ffa91247cb41d03e74da94f800aaed00aa0
Nikolai Tsepalov <tsepalov@yahoo.com>
2017-06-14 14:26:07 -0700

    Added the display method for a single dimension array

--- ArrayPractice/Program.cs
+++ ArrayPractice/Program.cs
@@ -8,17 +8,34 @@ namespace ArrayPractice
 {
     class Program
     {
+        static void Display(int[] array)
+        {            
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (i > 0)
+                {
+                    Console.Write(", ");
+                }
+                Console.Write(array[i]);            
+            }
+            Console.WriteLine();
+        }
         static void Display(int[,] array)
-        { 
+        {
+            
         }
 
         static void Main(string[] args)
         {
+            int[] test = {2, 668888886, 8};
+            Display(test);
+            return;
             int[,] array = new int[2, 6]
             {
                 { 10, 11, 12, 13, 14, 15 },
                 { 16, 17, 18, 19, 20, 21 }
             };
+            Display(array);
         }
     }
 }
